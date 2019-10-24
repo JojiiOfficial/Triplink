a7d765c5367641a1632ce03d1ba0b87134ffa606
Jojii <jojii@jojii.de>
2019-10-24 17:24:24 +0200

    added parsing

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func main() {
 			fmt.Println("Couldn't parse le (" + err.Error() + "): " + line)
 			continue
 		}
-		_ = logEntry
+		fmt.Println(*logEntry)
 	}
 }
 
@@ -46,7 +46,54 @@ type LogEntry struct {
 
 func parseLogEntry(content string) (*LogEntry, error) {
 	if !strings.Contains(content, "Tripwire") {
-		return nil, errors.New("no tripwire log")
+		return nil, errors.New("not a tripwire log")
 	}
-	return nil, nil
+	logItems := strings.Split(content, " ")
+	entry := &LogEntry{}
+	for _, val := range logItems {
+		handleLogEntry(val, entry)
+	}
+	dateString := logItems[0] + " " + logItems[1] + " " + logItems[2]
+	t, _ := time.Parse(time.Stamp, dateString)
+	t = t.AddDate(time.Now().Year(), 0, 0)
+	entry.Time = t
+	return entry, nil
+}
+
+func handleLogEntry(data string, entry *LogEntry) {
+	key, val, err := parseItem(data)
+	if err != nil {
+		return
+	}
+	switch key {
+	case "IN":
+		entry.In = val
+	case "OUT":
+		entry.Out = val
+	case "MAC":
+		entry.Mac = val
+	case "SRC":
+		entry.Src = val
+	case "DST":
+		entry.Dst = val
+	case "LEN":
+		entry.Len = val
+	case "TTL":
+		entry.TTL = val
+	case "ID":
+		entry.ID = val
+	case "PROTO":
+		entry.Protocol = val
+	}
+}
+
+func parseItem(item string) (string, string, error) {
+	if !strings.Contains(item, "=") {
+		return "", "", errors.New("no valid item")
+	}
+	data := strings.Split(item, "=")
+	if len(data) != 2 {
+		return "", "", errors.New("no data given")
+	}
+	return data[0], data[1], nil
 }
