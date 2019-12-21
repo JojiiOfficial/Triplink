ad4a063226c26b830677298bed6bafe64c30d0b2
jojiiofficial <jojii@jojii.de>
2019-12-21 13:38:20 +0100

    added version command -> close #25

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 
 	"github.com/mkideal/cli"
 )
@@ -10,27 +11,34 @@ import (
 var help = cli.HelpCommand("display help information")
 var logPrefix = ""
 var showTimeInLog = true
+var version = "0.3"
 
 type argT struct {
 	cli.Helper
+	Version bool `cli:"v,version" usage:"Show version"`
 }
 
 var root = &cli.Command{
 	Argv: func() interface{} { return new(argT) },
 	Fn: func(ctx *cli.Context) error {
-		fmt.Println("Commands:\n\n" +
-			"help           display help information\n" +
-			"createConfig   Create a new configuration file (aliases cc,cconf,createconf,createconfig)\n" +
-			"editConfig     Edit a configuration file (aliases econf,editconfig,ec,editc,edconf)\n" +
-			"deleteConfig   Delete a configuration file (aliases delconf,deleteconfig,dc,dconf,delc)\n" +
-			"viewConfig     View a configuration file (aliases vconf,vc,viewc,showconf,showconfig,config,conf,confshow,confview)\n" +
-			"install        Install a cronjob easily to automate the updating/reporting process\n" +
-			"update         Download and apply IP filter(aliases u,upd,update)\n" +
-			"ipinfo         Show info for an IP (aliases info,showip,ipdata,ipd,ii)\n" +
-			"restore        restore ipset and iptables (aliases res,restore,rest)\n" +
-			"backup         backups ipset(-s) and (iptables with -t) (aliases b,bak,backup)\n" +
-			"deletebackup   delete backups from ipset(-s) and (iptables -t) (aliases db,delbak,delbackup,deleteb,dback,delb)\n" +
-			"report         Reports all changes (aliases r,report,repo)")
+		argv := ctx.Argv().(*argT)
+		if argv.Version {
+			fmt.Println("Triplink V."+version, runtime.GOOS+"/"+runtime.GOARCH)
+		} else {
+			fmt.Println("Commands:\n\n" +
+				"help           display help information\n" +
+				"createConfig   Create a new configuration file (aliases cc,cconf,createconf,createconfig)\n" +
+				"editConfig     Edit a configuration file (aliases econf,editconfig,ec,editc,edconf)\n" +
+				"deleteConfig   Delete a configuration file (aliases delconf,deleteconfig,dc,dconf,delc)\n" +
+				"viewConfig     View a configuration file (aliases vconf,vc,viewc,showconf,showconfig,config,conf,confshow,confview)\n" +
+				"install        Install a cronjob easily to automate the updating/reporting process\n" +
+				"update         Download and apply IP filter(aliases u,upd,update)\n" +
+				"ipinfo         Show info for an IP (aliases info,showip,ipdata,ipd,ii)\n" +
+				"restore        restore ipset and iptables (aliases res,restore,rest)\n" +
+				"backup         backups ipset(-s) and (iptables with -t) (aliases b,bak,backup)\n" +
+				"deletebackup   delete backups from ipset(-s) and (iptables -t) (aliases db,delbak,delbackup,deleteb,dback,delb)\n" +
+				"report         Reports all changes (aliases r,report,repo)")
+		}
 		return nil
 	},
 }
