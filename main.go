df96c046df4a57230d11ecbd8cc701c4c38dc643
jojiiofficial <jojii@jojii.de>
2019-12-30 17:57:10 +0100

    improved root command overview

--- main.go
+++ main.go
@@ -27,19 +27,19 @@ var root = &cli.Command{
 			fmt.Println("Triplink V."+version, runtime.GOOS+"/"+runtime.GOARCH)
 		} else {
 			fmt.Println("Commands:\n\n" +
-				"help           display help information\n" +
-				"createConfig   Create a new configuration file (aliases cc,cconf,createconf,createconfig)\n" +
-				"editConfig     Edit a configuration file (aliases econf,editconfig,ec,editc,edconf)\n" +
-				"deleteConfig   Delete a configuration file (aliases delconf,deleteconfig,dc,dconf,delc)\n" +
-				"viewConfig     View a configuration file (aliases vconf,vc,viewc,showconf,showconfig,config,conf,confshow,confview)\n" +
-				"install        Install a cronjob easily to automate the updating/reporting process\n" +
-				"update         Download and apply IP filter(aliases u,upd,update)\n" +
-				"rules          change iptable rules created by triplink(aliases rule,rul)\n" +
-				"ipinfo         Show info for an IP (aliases info,showip,ipdata,ipd,ii)\n" +
-				"restore        restore ipset and iptables (aliases res,restore,rest)\n" +
-				"backup         backups ipset(-s) and (iptables with -t) (aliases b,bak,backup)\n" +
-				"deletebackup   delete backups from ipset(-s) and (iptables -t) (aliases db,delbak,delbackup,deleteb,dback,delb)\n" +
-				"report         Reports all changes (aliases r,report,repo)")
+				"  help           display help information\n" +
+				"  createConfig   Create a new configuration file (aliases cc,cconf,createconf,createconfig)\n" +
+				"  editConfig     Edit a configuration file (aliases econf,editconfig,ec,editc,edconf)\n" +
+				"  deleteConfig   Delete a configuration file (aliases delconf,deleteconfig,dc,dconf,delc)\n" +
+				"  viewConfig     View a configuration file (aliases vconf,vc,viewc,showconf,showconfig,config,conf,confshow,confview)\n" +
+				"  install        Install a cronjob easily to automate the updating/reporting process\n" +
+				"  update         Download and apply IP filter(aliases u,upd,update)\n" +
+				"  rules          change iptable rules created by triplink(aliases rule,rul)\n" +
+				"  ipinfo         Show info for an IP (aliases info,showip,ipdata,ipd,ii)\n" +
+				"  restore        restore ipset and iptables (aliases res,restore,rest)\n" +
+				"  backup         backups ipset(-s) and (iptables with -t) (aliases b,bak,backup)\n" +
+				"  deletebackup   delete backups from ipset(-s) and (iptables -t) (aliases db,delbak,delbackup,deleteb,dback,delb)\n" +
+				"  report         Reports all changes (aliases r,report,repo)")
 		}
 		return nil
 	},
